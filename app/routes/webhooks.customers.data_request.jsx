75a0218660ff81e786a9c7007f0b93cc5c5fcc4f
aahanatechpro <info@aahanatechnologies.com>
2026-04-09 16:39:23 +0530

    Add webhook

---
+++ app/routes/webhooks.customers.data_request.jsx
@@ -0,0 +1,7 @@
+import { authenticate } from "../shopify.server";
+
+export const action = async ({ request }) => {
+  await authenticate.webhook(request);
+
+  return new Response(null, { status: 200 });
+};
